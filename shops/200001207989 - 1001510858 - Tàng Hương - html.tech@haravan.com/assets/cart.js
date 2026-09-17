77b0d395e7a315adf2a5770de60bce110720017a
Nhan Nguyen <nhanbs1997@gmail.com>
2026-09-17 11:14:15 +0700

    Haravan theme: Tàng Hương (org 200001207989, theme 1001510858) - 2 file(s)

--- shops/200001207989 - 1001510858 - Tàng Hương - html.tech@haravan.com/assets/cart.js
+++ shops/200001207989 - 1001510858 - Tàng Hương - html.tech@haravan.com/assets/cart.js
@@ -70,6 +70,11 @@ class RewardsBar extends HTMLElement {
     this.classList.remove("opacity-0");
   }
   update(cartRewards) {
+    if (!cartRewards) {
+      this.innerHTML = "";
+      delete this.dataset.total;
+      return;
+    }
     this.dataset.total = cartRewards.dataset.total;
     this.innerHTML = cartRewards.innerHTML;
     this.init();
